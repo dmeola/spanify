68ef986e68b0448d733b1cf00441858cfdba9f8f
Daniel Meola <dmeola@gmail.com>
2014-10-28 15:31:37 -0400

    logic update
    spanify spanified element instead of child

--- spanify.js
+++ spanify.js
@@ -2,9 +2,9 @@ function replaceContentInContainer(matchClass) {
     var elems = document.getElementsByTagName('*'), i;
     for (i in elems) {
         if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ') > -1) {
-        	org_html = elems[i].innerHTML;
+        	org_html = elems[i].parentNode.innerHTML;
 			new_html = "<span>" + org_html + "</span>";
-            elems[i].innerHTML = new_html;
+            elems[i].parentNode.innerHTML = new_html.replace("spanify", "spanned");
         }
     }
 }
